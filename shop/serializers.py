79e2d85ddfa660ea383af073017aabca5c3707f4
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-11-27 14:27:07 +0100

    P1C5 course script added

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from shop.models import Category
+from shop.models import Category, Product
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'description', 'date_created', 'date_updated']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'category', 'date_created', 'date_updated']
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,25 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from shop.models import Category
-from shop.serializers import CategorySerializer
+from shop.models import Category, Product
+from shop.serializers import CategorySerializer, ProductSerializer
 
 class CategoryViewset(ReadOnlyModelViewSet):
  
     serializer_class = CategorySerializer
  
     def get_queryset(self):
-        return Category.objects.all()
+        return Category.objects.filter(active=True)
+    
+class ProductViewset(ReadOnlyModelViewSet):
+ 
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(active=True)
+
+        category_id = self.request.GET.get('category_id')
+
+        if category_id is not None:
+            queryset = queryset.filter(category_id=category_id)
+    
+        return queryset
