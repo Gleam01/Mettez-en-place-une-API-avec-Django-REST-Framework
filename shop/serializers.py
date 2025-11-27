ee83f82f55617389bd89dc27d97f19d647849b58
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-11-27 16:06:11 +0100

    P2C1 course script added

--- shop/serializers.py
+++ shop/serializers.py
@@ -2,15 +2,31 @@ from rest_framework import serializers
 from shop.models import Category, Product, Article
 
 class CategorySerializer(serializers.ModelSerializer):
+    # En utilisant un `SerializerMethodField', il est nécessaire d'écrire une méthode
+    # nommée 'get_XXX' où XXX est le nom de l'attribut, ici 'products'
+    products = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description', 'date_created', 'date_updated']
+        fields = ['id', 'name', 'description', 'date_created', 'date_updated', 'products']
+
+    def get_products(self, instance):
+        # Le paramètre 'instance' est l'instance de la catégorie consultée.
+        # Dans le cas d'une liste, cette méthode est appelée autant de fois qu'il y a
+        # d'entités dans la liste
+
+        # On applique le filtre sur notre queryset pour n'avoir que les produits actifs
+        queryset = instance.products.filter(active=True)
+        # Le serializer est créé avec le queryset défini et toujours défini en tant que many=True
+        serializer = ProductSerializer(queryset, many=True)
+
+        # la propriété '.data' est le rendu de notre serializer que nous retournons ici
+        return serializer.data
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'category', 'date_created', 'date_updated']
-
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
