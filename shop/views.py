006ea1d4c3e5da20933390f48f62d8f3512e6c46
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-11-27 14:35:37 +0100

    P1C5 exercise done

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,10 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from shop.models import Category, Product
-from shop.serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product, Article
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
 
 
 class CategoryViewset(ReadOnlyModelViewSet):
-
     serializer_class = CategorySerializer
 
     def get_queryset(self):
@@ -13,7 +12,6 @@ class CategoryViewset(ReadOnlyModelViewSet):
 
 
 class ProductViewset(ReadOnlyModelViewSet):
-
     serializer_class = ProductSerializer
 
     def get_queryset(self):
@@ -21,4 +19,14 @@ class ProductViewset(ReadOnlyModelViewSet):
         category_id = self.request.GET.get('category_id')
         if category_id:
             queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+class ArticleViewset(ReadOnlyModelViewSet):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(active=True)
+        product_id = self.request.GET.get('product_id')
+        if product_id:
+            queryset = queryset.filter(product_id=product_id)
         return queryset
