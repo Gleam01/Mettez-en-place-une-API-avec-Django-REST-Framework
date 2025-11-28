c5dc3ff2a5bf64edc9daaa0de7149d48a39d6630
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-11-28 09:38:17 +0100

    P2C3 course script added

--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,32 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from shop.models import Category, Product, Article
 from shop.serializers import CategoryListSerializer, CategoryDetailSerializer, ProductSerializer, ArticleSerializer
 
-class CategoryViewset(ReadOnlyModelViewSet):
+class MultipleSerializerMixin:
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        
+        return super().get_serializer_class()
+
+class CategoryViewset(MultipleSerializerMixin, ReadOnlyModelViewSet):
  
     serializer_class = CategoryListSerializer
     detail_serializer_class = CategoryDetailSerializer
  
     def get_queryset(self):
         return Category.objects.filter(active=True)
-    
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        
-        return super().get_serializer_class()
+
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        instance = self.get_object()
+        instance.disable()
+        return Response()
     
 class ProductViewset(ReadOnlyModelViewSet):
  
